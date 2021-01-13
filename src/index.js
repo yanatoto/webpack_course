232bb9d6698016035157fdc2d377a3d8988e2e5a
Yana <yanas@bluewin.ch>
2021-01-13 22:37:18 +0100

    Add json.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Post from './Post'
-import './styles/styles.css'
+import json from'./assets/json'
+import './style/style.css'
 
 const post = new Post('Webpack Post Title')
 console.log('Post to String:', post.toString())
+console.log('JSON:', json)
