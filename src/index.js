0baddf8f5e9311a4751e1f01cc1706e31bfa9031
Yana <yanas@bluewin.ch>
2021-01-16 21:46:22 +0100

    Install xml-loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Post from './Post'
-import json from'./assets/json'
+import json from'./assets/json.json'
+import xml from './assets/data.xml'
 import WebpackLogo from './assets/webpack-logo.png'
 import './style/style.css'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 console.log('Post to String:', post.toString())
 console.log('JSON:', json)
+console.log('XML:', xml)
